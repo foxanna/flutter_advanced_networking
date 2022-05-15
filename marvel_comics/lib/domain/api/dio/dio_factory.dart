8d47a94650f693dea85ee66d972f2a4f5638c24d
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-15 17:24:35 +0300

    32. Create DioFactory

---
+++ marvel_comics/lib/domain/api/dio/dio_factory.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class DioFactory {
+  const DioFactory(this._baseUrl);
+
+  final String _baseUrl;
+
+  Dio create() => Dio(_createBaseOptions());
+
+  BaseOptions _createBaseOptions() => BaseOptions(
+        baseUrl: _baseUrl,
+        receiveTimeout: 15000,
+        sendTimeout: 15000,
+        connectTimeout: 5000,
+      );
+}

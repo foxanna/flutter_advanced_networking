47469e8c9a6f706ba8414368a53c8bbbddbb45b1
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-15 19:22:46 +0300

    45. Declare baseUrl in MarvelApiConsts

--- marvel_comics/lib/domain/api/marvel_api_consts.dart
+++ marvel_comics/lib/domain/api/marvel_api_consts.dart
@@ -6,4 +6,6 @@ abstract class MarvelApiConsts {
   // TODO: put your private key to defaultValue
   static const privateKey =
       String.fromEnvironment('PRIVATE_API_KEY', defaultValue: '');
+
+  static const baseUrl = 'https://gateway.marvel.com:443/v1/public';
 }

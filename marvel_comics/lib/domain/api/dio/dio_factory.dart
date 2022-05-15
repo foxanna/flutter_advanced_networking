e91c48c12e9aadcc7723bc04d711fc1d293f335e
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-15 23:31:03 +0300

    61. Implement proxy support for dio

--- marvel_comics/lib/domain/api/dio/dio_factory.dart
+++ marvel_comics/lib/domain/api/dio/dio_factory.dart
@@ -1,26 +1,32 @@
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:marvel_comics/domain/api/dio/interceptors/marvel_api_auth_interceptor.dart';
+import 'package:marvel_comics/domain/api/dio/interceptors/proxy_interceptor.dart';
+import 'package:marvel_comics/domain/api/dio/proxy/proxy_finder.dart';
 
 class DioFactory {
   const DioFactory(
     this._baseUrl,
     this._apiAuthInterceptor,
+    this._proxyInterceptor,
+    this._proxyFinder,
   );
 
   final String _baseUrl;
   final MarvelApiAuthInterceptor _apiAuthInterceptor;
+  final ProxyInterceptor _proxyInterceptor;
+  final ProxyFinder _proxyFinder;
 
   Dio create() => Dio(_createBaseOptions())
     ..interceptors.addAll([
       _apiAuthInterceptor,
+      _proxyInterceptor,
     ])
     ..httpClientAdapter = _createAdapter();
 
   HttpClientAdapter _createAdapter() => DefaultHttpClientAdapter()
-    ..onHttpClientCreate = (client) => client..findProxy = _findProxy;
-
-  String _findProxy(Uri url) => 'DIRECT';
+    ..onHttpClientCreate =
+        (client) => client..findProxy = _proxyFinder.findProxy;
 
   BaseOptions _createBaseOptions() => BaseOptions(
         baseUrl: _baseUrl,

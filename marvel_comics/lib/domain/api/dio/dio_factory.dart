e458c4402333fdbedbe479a51c0421ca44166b9f
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-15 19:14:38 +0300

    43. Attach MarvelApiAuthInterceptor to dio

--- marvel_comics/lib/domain/api/dio/dio_factory.dart
+++ marvel_comics/lib/domain/api/dio/dio_factory.dart
@@ -1,13 +1,19 @@
 import 'package:dio/dio.dart';
+import 'package:marvel_comics/domain/api/dio/interceptors/marvel_api_auth_interceptor.dart';
 
 class DioFactory {
   const DioFactory(
     this._baseUrl,
+    this._apiAuthInterceptor,
   );
 
   final String _baseUrl;
+  final MarvelApiAuthInterceptor _apiAuthInterceptor;
 
-  Dio create() => Dio(_createBaseOptions());
+  Dio create() => Dio(_createBaseOptions())
+    ..interceptors.addAll([
+      _apiAuthInterceptor,
+    ]);
 
   BaseOptions _createBaseOptions() => BaseOptions(
         baseUrl: _baseUrl,

2dc24ea0290dbab83541e9a792cb720e2cee6c81
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-17 22:28:13 +0300

    73. Reimplement MarvelComicsApi with retrofit

--- marvel_comics/lib/domain/api/marvel_comics_api.dart
+++ marvel_comics/lib/domain/api/marvel_comics_api.dart
@@ -2,24 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:marvel_comics/domain/api/model/marvel_api_response.dart';
 import 'package:marvel_comics/domain/model/marvel_comic.dart';
 import 'package:marvel_comics/domain/model/marvel_paginated_list.dart';
+import 'package:retrofit/retrofit.dart';
 
-class MarvelComicsApi {
-  const MarvelComicsApi(this._dio);
+part 'marvel_comics_api.g.dart';
 
-  final Dio _dio;
+@RestApi()
+abstract class MarvelComicsApi {
+  factory MarvelComicsApi(Dio dio) = _MarvelComicsApi;
 
-  Future<MarvelApiResponse<MarvelPaginatedList<MarvelComic>>>
-      getComics() async {
-    final response = await _dio.get<Map<String, dynamic>>('/comics');
-    final json = response.data!;
-    final apiResponse =
-        MarvelApiResponse<MarvelPaginatedList<MarvelComic>>.fromJson(
-      json,
-      (json) => MarvelPaginatedList.fromJson(
-        json as Map<String, dynamic>,
-        (json) => MarvelComic.fromJson(json as Map<String, dynamic>),
-      ),
-    );
-    return apiResponse;
-  }
+  @GET('/comics')
+  Future<MarvelApiResponse<MarvelPaginatedList<MarvelComic>>> getComics();
 }
---
+++ marvel_comics/lib/domain/api/marvel_comics_api.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'marvel_comics_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _MarvelComicsApi implements MarvelComicsApi {
+  _MarvelComicsApi(this._dio, {this.baseUrl});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<MarvelApiResponse<MarvelPaginatedList<MarvelComic>>>
+      getComics() async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<MarvelApiResponse<MarvelPaginatedList<MarvelComic>>>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/comics',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = MarvelApiResponse<MarvelPaginatedList<MarvelComic>>.fromJson(
+      _result.data!,
+      (json) => MarvelPaginatedList<MarvelComic>.fromJson(
+        json as Map<String, dynamic>,
+        (json) => MarvelComic.fromJson(json as Map<String, dynamic>),
+      ),
+    );
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}

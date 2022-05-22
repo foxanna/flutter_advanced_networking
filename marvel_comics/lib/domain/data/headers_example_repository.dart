43be575615a50c17a2e6e2a1da5470e61353854b
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-22 23:27:20 +0300

    97. Use SecretDataRepository in one of HeadersExampleRepository requests

--- marvel_comics/lib/domain/data/headers_example_repository.dart
+++ marvel_comics/lib/domain/data/headers_example_repository.dart
@@ -1,15 +1,27 @@
 import 'package:marvel_comics/domain/api/headers_example_api.dart';
+import 'package:marvel_comics/domain/data/secret_data_repository.dart';
 
 class HeadersExampleRepository {
-  const HeadersExampleRepository(this._api);
+  const HeadersExampleRepository(
+    this._api,
+    this._secretDataRepository,
+  );
 
   final HeadersExampleApi _api;
+  final SecretDataRepository _secretDataRepository;
 
   Future<String> getA() => _api.requestA();
 
   Future<String> getB(String parameter) => _api.requestB(parameter: parameter);
 
-  Future<String> getC() => _api.requestC(securityHeaderValue: 'secretData');
+  Future<String> getC() async {
+    final secretData = await _secretDataRepository.getSecretData();
+    if (secretData != null) {
+      return _api.requestC(securityHeaderValue: secretData);
+    } else {
+      throw Exception();
+    }
+  }
 
   Future<String> getD() => _api.requestD(data: <String, dynamic>{});
 }

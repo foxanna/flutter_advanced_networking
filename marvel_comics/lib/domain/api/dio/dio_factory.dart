f8ffa50792bd2feb6e61c40b5e52320eb6078d8e
Anna (Domashych) Leushchenko <anna.domashych@gmail.com>
2022-05-22 12:14:23 +0300

    38. Remove examples

--- marvel_comics/lib/domain/api/dio/dio_factory.dart
+++ marvel_comics/lib/domain/api/dio/dio_factory.dart
@@ -1,24 +1,18 @@
 import 'package:dio/dio.dart';
-import 'package:marvel_comics/domain/api/dio/interceptors/append_timestamp_interceptor.dart';
 
 class DioFactory {
   const DioFactory(
     this._baseUrl,
-    this._appendTimestampInterceptor,
   );
 
   final String _baseUrl;
-  final AppendTimestampInterceptor _appendTimestampInterceptor;
 
-  Dio create() =>
-      Dio(_createBaseOptions())..interceptors.add(_appendTimestampInterceptor);
+  Dio create() => Dio(_createBaseOptions());
 
   BaseOptions _createBaseOptions() => BaseOptions(
         baseUrl: _baseUrl,
         receiveTimeout: 15000,
         sendTimeout: 15000,
         connectTimeout: 5000,
-        queryParameters: <String, dynamic>{'parameter1': 'value1'},
-        headers: <String, dynamic>{'header1': 'value1'},
       );
 }
